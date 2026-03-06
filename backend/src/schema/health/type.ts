ea64f7666f5b341c9faba90ed75767bfbc305f9e
geoff-unit <geoff@unit.inc>
2026-03-06 10:14:24 -0500

    remove unused health query

--- backend/src/schema/health/type.ts
+++
@@ -1,8 +0,0 @@
-import { builder } from '@/schema/builder';
-
-export const HealthStatus = builder.simpleObject('HealthStatus', {
-  fields: (t) => ({
-    status: t.string({ nullable: false }),
-    timestamp: t.string({ nullable: false }),
-  }),
-});
