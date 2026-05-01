848a756104dae6eb8edb5c2b5ba9c87149ace4e2
geoffb273 <geoffb27@gmail.com>
2026-05-01 14:09:08 -0400

    app version

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -3,7 +3,7 @@ import { type ExpoConfig } from 'expo/config';
 export default (): ExpoConfig => ({
   name: 'Game Brain',
   slug: 'game-brain',
-  version: '1.1.4',
+  version: '1.1.5',
   orientation: 'portrait',
   scheme: 'games-brain',
   userInterfaceStyle: 'automatic',
